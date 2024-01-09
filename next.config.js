d6632bf97368ca7d87da0df068b5ea57d23ca870
Abdulrahman Ismail <abdulrahmanismail987@gmail.com>
2024-01-09 15:10:40 +0300

    fixing typescript erorr

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const redirects = require('./redirects')
 
 const nextConfig = {
   typescript: {
-    ignoreBulidErrors: true,
+    ignoreBuildErrors: true,
   },
   reactStrictMode: true,
   swcMinify: true,
